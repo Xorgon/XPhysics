1ab9a911f1a511545e5d6be5ef967ce3f8d411b9
Elijah Andrews <norman.reggox@gmail.com>
2014-11-22 21:19:31 +0000

    Added the acceleration class. Restructuring.

--- src/main/java/XPhysics.java
+++ src/main/java/XPhysics.java
@@ -1,3 +1,4 @@
+import Mechanics.Acceleration;
 import Mechanics.Velocity;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
@@ -10,14 +11,29 @@ public class XPhysics {
 
     private XPhysics instance = this;
     private Velocity velocity;
+    private Acceleration acceleration;
     private Plugin plugin;
 
     public void XPhysics(Plugin plugin) {
-        plugin.getServer().getPluginManager().registerEvents(new XPListeners(this, velocity), plugin);
         this.plugin = plugin;
+        this.velocity = new Velocity();
+        this.acceleration = new Acceleration();
+        plugin.getServer().getPluginManager().registerEvents(new XPListeners(this), plugin);
     }
 
     public XPhysics getInstance() {
         return instance;
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    public Velocity getVelocity() {
+        return velocity;
+    }
+
+    public Acceleration getAcceleration() {
+        return acceleration;
     }
 }
